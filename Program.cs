3651791b2e5e83b576865e9025be78eea9eca58a
camili costa <camili.costa07@gmail.com>
2023-10-27 21:38:43 -0300

    adicionar e finalizar

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+int numeroUm;
+int numeroDois;
+int aux;
+do
+
+ {
+    Console.WriteLine("Inicio:");
+    Int32.TryParse(Console.ReadLine(),out numeroUm);
+    
+    Console.WriteLine("Fim:");
+    Int32.TryParse(Console.ReadLine(),out numeroDois);
+ }
+ while (numeroUm > numeroDois);
+ Console.WriteLine();
+ Console.WriteLine($"Sequencia de numeros {numeroUm} a {numeroDois}");
+
+ while (numeroUm <= numeroDois)
+ {
+    Console.WriteLine($"{numeroUm}");
+numeroUm++;
+
+ }
+
+ aux= numeroUm;
+ {
+Console.WriteLine($"{aux}");
+ aux++;
+ 
+ }
+ 
